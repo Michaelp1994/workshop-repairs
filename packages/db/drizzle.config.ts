971bb9ca7036e70ede8e97a22a9ee3d6d0f0b372
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-08-05 16:40:08 -0300

    update to vercel postgres driver and vercel env variables

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,13 +1,14 @@
 import { type Config } from "drizzle-kit";
 
-if (!process.env["DATABASE_URL"]) {
-  throw new Error("DATABASE_URL is not set");
+if (!process.env["POSTGRES_URL"]) {
+  throw new Error("POSTGRES_URL is not set");
 }
 
 export default {
   schema: "./src/schemas/*.ts",
   dialect: "postgresql",
+
   dbCredentials: {
-    url: process.env["DATABASE_URL"],
+    url: process.env["POSTGRES_URL"],
   },
 } satisfies Config;
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,23 +1,20 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+// import { drizzle } from "drizzle-orm/postgres-js";
+import { sql } from "@vercel/postgres";
+import { drizzle } from "drizzle-orm/vercel-postgres";
+
+// import postgres from "postgres";
 
 import { schema } from "./schema";
 
-/**
- * Cache the database connection in development. This avoids creating a new connection on every HMR
- * update.
- */
-const globalForDb = globalThis as unknown as {
-  conn: postgres.Sql | undefined;
-};
+// if (!process.env["DATABASE_URL"]) {
+//   throw Error("ensure env variables are set up (DATABASE_URL, NODE_ENV)");
+// }
 
-if (!process.env["DATABASE_URL"]) {
-  throw Error("ensure env variables are set up (DATABASE_URL, NODE_ENV)");
-}
+// const conn = postgres(process.env["DATABASE_URL"]);
+// if (process.env["NODE_ENV"] !== "production") globalForDb.conn = conn;
 
-const conn = globalForDb.conn ?? postgres(process.env["DATABASE_URL"]);
-if (process.env["NODE_ENV"] !== "production") globalForDb.conn = conn;
+export const db = drizzle(sql, { schema });
 
-export const db = drizzle(conn, { schema });
+// export const db = drizzle(conn, { schema });
 
 export type Database = typeof db;
