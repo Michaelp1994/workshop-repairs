04e0dd5d3c66570afed7a980a404b39f0dbfef18
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-11-08 11:25:45 +1000

    chore: cleanup old pages.

--- todo/repairStatusTypes/page.tsx
+++
@@ -1,9 +0,0 @@
-import RepairStatusTypesTable from "./RepairStatusTypesTable";
-
-export default function AllRepairStatusTypesPage() {
-  return (
-    <div>
-      <RepairStatusTypesTable />
-    </div>
-  );
-}
