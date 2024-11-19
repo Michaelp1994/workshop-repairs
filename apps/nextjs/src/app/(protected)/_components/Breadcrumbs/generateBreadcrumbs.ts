252358d55281f87a34a62ae97adc8e772aa55ad4
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-11-19 10:49:56 +1000

    fix: breadcrumbs now replaces "-" with a space.

--- apps/nextjs/src/app/(protected)/_components/Breadcrumbs/generateBreadcrumbs.ts
+++ apps/nextjs/src/app/(protected)/_components/Breadcrumbs/generateBreadcrumbs.ts
@@ -1,7 +1,8 @@
 export default function generateBreadcrumbs(layoutSegments: string[]) {
   return layoutSegments.map((segment, index) => {
+    const label = segment.replace(/-/g, " ");
     return {
-      label: segment,
+      label: label,
       href: `/${layoutSegments.slice(0, index + 1).join("/")}`,
     };
   });
