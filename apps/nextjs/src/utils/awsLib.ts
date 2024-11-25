8c04d99c5d8c8eedfce0f046e14c04f94ba2ee2e
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-11-25 17:06:09 +1000

    fix: added helper functions for s3 on client and server

--- apps/nextjs/src/utils/awsLib.ts
+++ apps/nextjs/src/utils/awsLib.ts
@@ -1,3 +1,10 @@
-import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 
 export const client = new S3Client({});
+
+export async function uploadImageToS3(file: File, url: string) {
+  await fetch(url, {
+    method: "PUT",
+    body: file,
+  });
+}
