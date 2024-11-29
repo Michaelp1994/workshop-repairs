abe12f1a7ea1434b9781ffd8f759c0ae4deef8e1
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-11-29 13:20:37 +1000

    ci: reset database before seeding, so staging environment always refresh data after synchronizing

--- packages/db/src/seed.ts
+++ packages/db/src/seed.ts
@@ -1,8 +1,11 @@
+import { reset } from "drizzle-seed";
+
 import asset_statuses from "../seed/asset_statuses.json";
 import repair_status_types from "../seed/repair_status_types.json";
 import repair_types from "../seed/repair_types.json";
 import user_types from "../seed/user_types.json";
 import { db } from "./index";
+import { schema } from "./tables";
 import { assetStatusTable } from "./tables/asset-status.sql";
 import { repairStatusTypeTable } from "./tables/repair-status-type.sql";
 import { repairTypeTable } from "./tables/repair-type.sql";
@@ -11,6 +14,7 @@ import { userTypeTable } from "./tables/user-type.sql";
 export async function handler() {
   console.log("Seeding database...");
   try {
+    await reset(db, schema);
     await db.transaction(async (t) => {
       await t.insert(assetStatusTable).values(asset_statuses);
       await t.insert(repairStatusTypeTable).values(repair_status_types);
