cb06da02f62e2043a0661f1f487c5d189d15a806
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-11-30 20:58:52 +1000

    fix: ci-testing (#49)
    * ci: remove deployment from github actions for the time being
    
    * ci: add autodeploy settings
    
    * ci: invoke migration and seed lambdas during deployment
    
    * ci: pnpm needs to be set up before nodejs for caching to work
    
    * build: only put postgres behind a proxy in production and 5s connection timeout for pg pool

--- sst.config.ts
+++ sst.config.ts
@@ -1,6 +1,22 @@
 /// <reference path="./.sst/platform/config.d.ts" />
 
 export default $config({
+  console: {
+    autodeploy: {
+      target(event) {
+        if (
+          event.type === "tag" &&
+          event.tag.startsWith("v") &&
+          event.action === "pushed"
+        ) {
+          return { stage: "production" };
+        }
+        if (event.type === "pull_request") {
+          return { stage: `pr-${event.number}` };
+        }
+      },
+    },
+  },
   app(input) {
     return {
       name: "workshop-repairs",
@@ -12,6 +28,7 @@ export default $config({
     const email = await import("./infra/email");
     const storage = await import("./infra/storage");
     const web = await import("./infra/web");
+
     return {
       Region: aws.getRegionOutput().name,
       nextjs: web.nextjs.url,
