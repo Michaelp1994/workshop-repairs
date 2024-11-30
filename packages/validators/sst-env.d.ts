4ea4538f0e8f23c23a10bbafce2c16db35fd766b
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-11-30 16:17:28 +1000

    chore: sst state

--- packages/validators/sst-env.d.ts
+++ packages/validators/sst-env.d.ts
@@ -5,5 +5,43 @@
 import "sst";
 export {};
 declare module "sst" {
-  export interface Resource {}
+  export interface Resource {
+    Bucket1: {
+      name: string;
+      type: "sst.aws.Bucket";
+    };
+    Email1: {
+      configSet: string;
+      sender: string;
+      type: "sst.aws.Email";
+    };
+    MigrationLambda: {
+      name: string;
+      type: "sst.aws.Function";
+    };
+    MyRouter: {
+      type: "sst.aws.Router";
+      url: string;
+    };
+    MyWeb: {
+      type: "sst.aws.Nextjs";
+      url: string;
+    };
+    Postgres: {
+      database: string;
+      host: string;
+      password: string;
+      port: number;
+      type: "sst.aws.Postgres";
+      username: string;
+    };
+    SeedLambda: {
+      name: string;
+      type: "sst.aws.Function";
+    };
+    Vpc1: {
+      bastion: string;
+      type: "sst.aws.Vpc";
+    };
+  }
 }
