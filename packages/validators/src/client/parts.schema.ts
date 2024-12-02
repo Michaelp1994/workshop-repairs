37a23fe371833ecd16e1966302c0c84ca0ccafee
Michael Poulgrain <michael.poulgrain@gmail.com>
2024-12-02 14:08:19 +1000

    feat: add part description to server and form validators

--- packages/validators/src/client/parts.schema.ts
+++ packages/validators/src/client/parts.schema.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const partFormSchema = z.object({
   name: z.string().min(3),
   partNumber: z.string().min(3),
+  description: z.string(),
 });
 
 export type PartFormInput = z.infer<typeof partFormSchema>;
@@ -10,4 +11,5 @@ export type PartFormInput = z.infer<typeof partFormSchema>;
 export const defaultPart: PartFormInput = {
   name: "",
   partNumber: "",
+  description: "",
 };
