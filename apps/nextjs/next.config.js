5b5940e072c33e7a5c3f38ae23a69d93d91ef8bc
Michael Poulgrain <michael.poulgrain@gmail.com>
2025-05-02 22:07:00 +1000

    build: ignore libraries in webpack build to remove UnhandledSchemeError error

--- apps/nextjs/next.config.js
+++ apps/nextjs/next.config.js
@@ -8,6 +8,14 @@ const config = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^pg-native$|^cloudflare:sockets$/,
+      }),
+    );
+    return config;
+  },
   experimental: {
     serverComponentsExternalPackages: [
       "@node-rs/argon2",
