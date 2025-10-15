af9a20fae3737d4f44a76655ff1bbe8598a9f2b5
Michael Poulgrain <michael.poulgrain@gmail.com>
2025-10-15 21:24:52 +1000

    feat: remove sst

--- infra/email.ts
+++
@@ -1,6 +0,0 @@
-export const email =
-  $app.stage === "production"
-    ? new sst.aws.Email("Email1", {
-        sender: "michael.poulgrain.aws@gmail.com",
-      })
-    : sst.aws.Email.get("Email1", "michael.poulgrain.aws@gmail.com");
