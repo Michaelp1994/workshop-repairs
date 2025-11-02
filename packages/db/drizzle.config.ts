720e9f1e6d8d677fa25bf41db04a828d573380b1
Michael Poulgrain <michael.poulgrain@gmail.com>
2025-11-02 20:40:29 +1000

    feat: move drizzle-kit to db repo and run studio on dev

--- packages/db/drizzle.config.ts
+++ packages/db/drizzle.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "drizzle-kit";
 
+if (
+  !process.env["POSTGRES_HOST"] ||
+  !process.env["POSTGRES_PORT"] ||
+  !process.env["POSTGRES_USER"] ||
+  !process.env["POSTGRES_PASSWORD"] ||
+  !process.env["POSTGRES_DB"]
+) {
+  throw Error("env vars not working for drizzle");
+}
+
 export default defineConfig({
   dialect: "postgresql",
-  schema: "./packages/db/src/tables/*.sql.ts",
-  out: "./packages/db/migrations",
+  schema: "./src/tables/*.sql.ts",
+  out: "./migrations",
   casing: "snake_case",
   dbCredentials: {
     host: process.env["POSTGRES_HOST"],
